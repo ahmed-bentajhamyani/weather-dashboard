32efa6b633a0f860b196cc96e7e44c9d2dc124dc
Ahmed Bentaj Hamyani <ahmedbentaj710@gmail.com>
2024-03-23 12:44:28 +0000

    set size full to search box in mobile

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -6,7 +6,7 @@ export default function SearchBox({ searchBoxOpen, searchResult }) {
     return (
         <>
             {searchBoxOpen ?
-                <div className='absolute top-12 rounded-3xl bg-black text-white w-3/4 border border-gray-700 z-10'>
+                <div className='absolute top-12 rounded-3xl bg-black text-white w-full md:w-3/4 border border-gray-700 z-10'>
                     {searchResult?.length >= 1 ?
                         <div className={`h-fit overflow-y-auto overflow-x-hidden p-2`}>
                             <WeatherCard city={searchResult[0]?.name} />
